d1572ccd92fd995a4df7fa20fb44f2bee2d795a2
Marina Grishina <marisupova@gmail.com>
2024-03-03 17:33:48 +0300

    correct cognitive complexity3

--- src/even.js
+++ src/even.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 import getRandomInclusive from './random.js';
+import name from './user-name.js';
 
 const isEven = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(`Hello, ${name}! \nAnswer "yes" if the number is even, otherwise answer "no".`);
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomInclusive(0, 100);
     console.log(`Question: ${number}`);
