81eea2efc854843a6e0243eea95d5e571116e764
Marina Grishina <marisupova@gmail.com>
2024-03-03 16:38:07 +0300

    even done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-import askAndGreeting from '../src/cli.js';
 import isEven from '../src/even.js';
 
 console.log('Welcome to the Brain Games!');
-askAndGreeting();
 isEven();
 
 // общий файл, оболочка
--- src/even.js
+++ src/even.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-import name from './cli.js';
 
 const getRandomInclusive = (min, max) => {
   const bottom = Math.ceil(min);
@@ -8,6 +7,8 @@ const getRandomInclusive = (min, max) => {
 };
 
 const isEven = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomInclusive(0, 100);
@@ -17,18 +18,18 @@ const isEven = () => {
       console.log('Correct!');
     } else if (number % 2 !== 0 && answer === 'yes') {
       console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
-      console.log('Let\'s try again, Bill!');
+      console.log(`Let's try again, ${name}!`);
       return;
     } else if (number % 2 === 0 && answer === 'no') {
       console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-      console.log('Let\'s try again, Bill!');
+      console.log(`Let's try again, ${name}!`);
       return;
     } else {
-      console.log('Let\'s try again, Bill!');
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
-  console.log(`'Congratulations, ${name}'`);
+  console.log(`Congratulations, ${name}`);
 };
 
 export default isEven;
