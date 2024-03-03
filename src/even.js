d36f75d459483c9e6b2042a24a842bfcd81a9eef
Marina Grishina <marisupova@gmail.com>
2024-03-03 17:23:06 +0300

    correct cognitive complexity2

--- src/even.js
+++ src/even.js
@@ -12,12 +12,10 @@ const isEven = () => {
     if ((number % 2 === 0 && answer === 'yes') || (number % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
     } else if (number % 2 !== 0 && answer === 'yes') {
-      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
-      console.log(`Let's try again, ${name}!`);
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
       return;
     } else if (number % 2 === 0 && answer === 'no') {
-      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-      console.log(`Let's try again, ${name}!`);
+      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
       return;
     } else {
       console.log(`Let's try again, ${name}!`);
