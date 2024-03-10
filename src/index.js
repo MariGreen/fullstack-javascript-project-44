c49a95fdd1d7a8977dc757282cec74b642b7bfdd
Marina Grishina <marisupova@gmail.com>
2024-03-10 22:34:17 +0300

    calc and even second refactoring

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import readlineSync from 'readline-sync';
+import check from './utilits';
+
 // логика всех игр
+const constractGame = (task, question, correctAnswer) => {
+  const maxRounds = 3;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(task);
+
+  for (let i = 0; i < maxRounds; i += 1) {
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    check(answer, correctAnswer);
+  }
+  console.log(`Congratulations, ${name}`);
+};
+
+export default constractGame;
