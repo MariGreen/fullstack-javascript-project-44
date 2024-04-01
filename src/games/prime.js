7945a8de8bde506802a194353ae7fd7d559e38b9
Marina Grishina <marisupova@gmail.com>
2024-04-01 13:59:55 +0300

    correct isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,14 @@ import constractGame from '../index.js';
 const isPrime = (number) => {
   if (number < 2) {
     return false;
-  } for (let i = 2; i <= number / 2; i += 1) {
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
     }
-  } return true;
+  }
+
+  return true;
 };
 
 const getTurnData = () => {
